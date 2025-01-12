2beee7c4f064565f11075ebc3b00b8f512913772
jeanrosetalen <rjean5097@gmail.com>
2025-01-12 22:01:32 +0800

    react static todos

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import ListTodos from './components/ListTodos'
+
+
+function App() {
+
+  const [user, setUser] = useState("Jean Rose")
+
+  return (
+    <>
+      <h1>Todo React App</h1>
+      {user ? (
+        <ListTodos user={user} />
+      ) : (
+        <p>You must login to view the todo lists.</p>
+      )}
+    
+    </> 
+  )
+}
+
+export default App
---
+++ src/components/ListTodos.jsx
@@ -0,0 +1,65 @@
+import { useEffect, useState } from 'react'
+
+const ListTodos = ({ user }) => {
+    const [loading, setLoading] = useState(true)
+    const [newTodo, setNewTodo] = useState('')
+    const [todos, setTodos] = useState([])
+
+    const fetchTodos = async (limit=5) => {
+        try {
+            const response = await fetch(`https://jsonplaceholder.typicode.com/todos?_limit=${limit}`)
+            const data = await response.json()
+
+            setTodos(data)
+            setLoading(false)
+        } catch(error) {
+            console.error(error.message)
+        }
+    }
+
+    useEffect(() => {
+        fetchTodos()
+
+    }, [])
+    
+    const handleToggleTodo = (id) => {
+        setTodos(todos.map(todo => (
+        todo.id === id ? {...todo, completed: !todo.completed } : todo
+        )))
+    }
+
+    const handleDeleteTodo = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id))
+    }
+
+    const handleNewTodo = () => {
+    setTodos([...todos, {id: todos.length + 1, title: newTodo, completed: false }])
+    setNewTodo('')
+    }
+
+    if (loading) return <>Loading ....</>
+
+    return (
+        <>
+            <div>
+                <h3>Welcome, {user}</h3>
+                <input type="text" value={newTodo} placeholder='Add todo' onChange={(e) => setNewTodo(e.target.value)} />
+                <button onClick={handleNewTodo}>Add</button>
+
+                {todos.map(todo => (
+                    <li key={todo.id}>
+                        <input type="checkbox" 
+                        checked={todo.completed}
+                        onChange={() => handleToggleTodo(todo.id)}/>
+                        {todo.completed ?<s>{todo.title}</s> : todo.title }
+                        <button onClick={() => handleDeleteTodo(todo.id)}>delete</button>
+                    </li>
+                ))}
+                
+            </div>
+        
+        </>
+    )
+}
+
+export default ListTodos
